3a48e129b899ee0b0a541eb221cd0dce0c738b7f
Varga-Mark-Ors <90794458+Varga-Mark-Ors@users.noreply.github.com>
2021-10-08 12:35:08 +0300

    Add files via upload

---
+++ source/Chapter5.cpp
@@ -0,0 +1,30 @@
+#include "std_lib_facilities.h"
+ 
+int main( )
+{
+cout << "Success!\n";
+cout << "Success!\n";
+cout << "Success" << "!\n";
+cout << "success" << "\n";
+int res = 7; vector<int> v(10); (v[5]!=res); cout << "Success!\n";
+ vector<int> v(10); (v[5] = 7); if (v[5]=7) cout << "Success!\n";
+if (bool cond=true) cout << "Success!\n"; else cout << "Fail!\n";
+bool c = true; if (c) cout << "Success!\n"; else cout << "Fail!\n";
+string s = "ape"; bool c = ("ape"==s); if (c) cout << "Success!\n";
+string s = "ape"; if (s=="ape") cout << "Success!\n";
+string s = "ape"; if (s=="ape") cout << "Success!\n";
+string s = "ape"; if (s=="ape") cout << "Success!\n";
+vector<char> v(5); for (int i=0; i<v.size(); ++i) ; cout << "Success!\n";
+vector<char> v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
+if (true) cout << "Success!\n"; else cout << "Fail!\n";
+int x = 2000; int c = x; if (c==2000) cout << "Success!\n";
+string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
+vector<int> v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+int i=0; int j = 9; while (i<10) ++i; if (j<i) cout << "Success!\n";
+int x = 3; double d = (2*x+0.5); if (d==2*x+0.5) cout << "Success!\n";
+string s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i];
+int i=0; while (i<=10) ++i; if (10<i) cout << "Success!\n";
+int x = 4;x=x-2; double d = 5/x; if (d=2*x+0.5) cout << "Success!\n";
+cout << "Success!\n";
+}
